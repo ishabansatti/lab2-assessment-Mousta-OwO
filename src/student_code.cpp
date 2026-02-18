f85c09008c425879422c4ab5c9c2cc8b914c05ed
Mousta-OwO <talhaomerusmani10@gmail.com>
2026-02-18 17:59:54 +0500

    Update student_code.cpp

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -36,6 +36,6 @@ Formula:
 float fahrenheitToCelsius(float fahrenheit) {
     //return celsius;
     float celsius = 0;
-    celsius = (fahrenheit - 32) * 5 / 9
+    celsius = (fahrenheit - 32) * 5 / 9;
     return celsius;
 }
